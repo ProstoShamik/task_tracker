32c6360009d20ce3116039990f90ff31539e4bb3
Shamil <Shamil@gmail.com>
2025-02-14 17:04:00 +0500

    Create TaskTrackerMenu TaskBase Task classes

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import json
+import uuid
 
 
 class TaskTrackerMenu(object):
-    pass
+    def __init__(self):
+        pass
 
 
 class TaskBase(object):
-    pass
+    def __init__(self):
+        pass
 
 
 class Task(object):
-    pass
+    def __init__(self, name: str, description: str):
+        self.task_info = {"name": name,
+                          "description": description,
+                          "id": uuid.uuid4()}
+
+    @property
+    def name(self):
+        return self.task_info["name"]
+
+    @name.setter
+    def name(self, value):
+        if value:
+            self.task_info["name"] = value
+
+    @property
+    def description(self):
+        return self.task_info["description"]
+
+    @description.setter
+    def description(self, value):
+        if value:
+            self.task_info["description"] = value
 
 
 def main():
